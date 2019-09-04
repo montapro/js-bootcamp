e269a9d41a8ebbe376a02417c123014d3425b030
Markus Krause <krause.markus@icloud.com>
2019-09-04 13:47:26 +0200

    Javascript arrays

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,10 +1,52 @@
 const account = {
   name: 'Andrew Mead',
-  expenses: []
+  expenses: [],
+  income: [],
+
+  // add expenses to the end of account.expenses array
+  addExpense: function(description, amount) {
+    this.expenses.push({
+      description: description,
+      amount: amount,
+    })
+  },
+  
+  // add incomes to the end of account.incomes array
+  addIncome: function(description, amount) {
+    this.income.push({
+      description: description,
+      amount: amount,
+    })
+  },
+
+  // Count together all expenses
+  getAccountSummary: function() {
+    // Declare totalExpenses for 0 expenses
+    let totalExpenses = 0
+    let totalIncomes = 0
+    let balance = 0
+
+    // Add all amount together in account.expenses array to totalExpenses variable
+    this.expenses.forEach(function(expense) {
+      totalExpenses += expense.amount
+    })
+
+    
+    // Add all amount together in account.income array to totalIncome variable
+    this.income.forEach(function(income) {
+      totalIncomes += income.amount
+    })
+    
+    // Calculate balance
+    balance = totalIncomes - totalExpenses
+
+    // Return Summary
+    return `${this.name} has a balance of $${balance}. $${totalIncomes} in income. $${totalExpenses} in expenses.`
+  },
 }
 
-
-
-account.add.Expense('Rent', 950)
-account.add.Expense('Coffee', 2)
+account.addExpense('Rent', 950)
+account.addExpense('Coffee', 2)
+account.addIncome('Job', 1000)
 console.log(account.getAccountSummary())
+
