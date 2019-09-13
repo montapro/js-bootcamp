6fb2e24ba61310059fb2f804da235c6bfdd5b8dc
Markus Krause <krause.markus@icloud.com>
2019-09-13 14:46:21 +0200

    7 checkboxes, 7 dropdowns

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -23,13 +23,24 @@ const todos = [
 
 const filters = {
   searchText: '',
+  hideCompleted: false,
 }
 
+// Render function for adding todos to the todo app
 const renderTodos = (todos, filters) => {
   // Filter todos by searchText
-  const filteredTodos = todos.filter(todo =>
-    todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-  )
+  const filteredTodos = todos.filter(todo => {
+    // Check if filtered text is in todo
+    const searchTextMatch = todo.text
+      .toLowerCase()
+      .includes(filters.searchText.toLowerCase())
+
+    // Check if todo.completed is false or filters.hideCompleted is false
+    const hideCompletedMatch = !todo.completed || !filters.hideCompleted
+
+    // Check if searchTextMatch and hideCompletedMatch are true, then return true
+    return searchTextMatch && hideCompletedMatch
+  })
 
   // Filter todos by completed
   const incompleteTodos = filteredTodos.filter(todo => !todo.completed)
@@ -39,8 +50,10 @@ const renderTodos = (todos, filters) => {
 
   // Create summary variable
   const summary = document.createElement('h2')
+
   // Include Content to summary variable
   summary.textContent = `You have ${incompleteTodos.length} todos left`
+
   // Append summary to #todos div in DOM
   document.querySelector('#todos').appendChild(summary)
 
@@ -55,18 +68,34 @@ const renderTodos = (todos, filters) => {
 // Initialize renderTodos
 renderTodos(todos, filters)
 
-// Listen for new todo creation
-document.querySelector('#add-todo').addEventListener('click', e => {
-  console.log('Added new todo')
-})
-
-// Listen for change input
-document.querySelector('#new-todo-text').addEventListener('input', e => {
-  console.log(e.target.value)
-})
-
 // Listen for change search-text
 document.querySelector('#search-text').addEventListener('input', e => {
   filters.searchText = e.target.value
   renderTodos(todos, filters)
+})
+
+// Listen for checkbox hide-completed checked
+document.querySelector('#hide-completed').addEventListener('change', e => {
+  filters.hideCompleted = e.target.checked
+  renderTodos(todos, filters)
+})
+
+// Adding todos
+document.querySelector('#new-todo').addEventListener('submit', e => {
+  // prevent default behavior of form submit
+  e.preventDefault()
+
+  // Push newTodo object to todos array if its text value is not empty string
+  if (e.target.elements.text.value != '') {
+    todos.push({
+      text: e.target.elements.text.value,
+      completed: false,
+    })
+
+    // renderTodos function once again
+    renderTodos(todos, filters)
+  }
+
+  // clear input value
+  e.target.elements.text.value = ''
 })
