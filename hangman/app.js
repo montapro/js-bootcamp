783500685315e5a15ecdb0f52fb1937a6eb1ceae
Markus Krause <krause.markus@icloud.com>
2019-10-14 16:53:29 +0200

    add status to hangman in console

--- hangman/app.js
+++ hangman/app.js
@@ -1,28 +1,15 @@
-// Primitive value: string, number, boolean, null, undefined
-
-// Object: myObject --> Object.prototype --> null
-// Array: myArray --> Array.prototype --> Object.prototype --> null
-// Function: myFunc --> Function.prototype --> Object.prototype --> null
-// String: myString --> String.prototype --> Object.prototype --> null
-// Number: myNumber --> Number.prototype --> Object.prototype --> null
-// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
-
-// const product = 'Computer'
-// console.log(product)
-
-// const otherProduct = new String('Phone')
-// console.log(otherProduct)
-
 const puzzleEl = document.getElementById('puzzle')
 const guessesEl = document.getElementById('guesses')
 const game1 = new Hangman('Cat', 2)
 
 puzzleEl.textContent = game1.getPuzzle()
 guessesEl.textContent = game1.remainingGuesses
+console.log(game1.status)
 
 window.addEventListener('keypress', e => {
   const guess = String.fromCharCode(e.charCode)
   game1.makeGuess(guess)
   puzzleEl.textContent = game1.getPuzzle()
   guessesEl.textContent = game1.remainingGuesses
+  console.log(game1.status)
 })
