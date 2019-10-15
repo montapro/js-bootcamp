1bb9d3e53c34040cc8b620efee62a7416302dfb4
Markus Krause <krause.markus@icloud.com>
2019-10-15 17:08:42 +0200

    fix issue in hangman by calculateStatus

--- hangman/app.js
+++ hangman/app.js
@@ -1,6 +1,6 @@
 const puzzleEl = document.getElementById('puzzle')
 const guessesEl = document.getElementById('guesses')
-const game1 = new Hangman('Cat', 2)
+const game1 = new Hangman('Car parts', 2)
 
 puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -6,8 +6,8 @@ class Hangman {
     this.status = 'playing'
   }
   calculateStatus() {
-    const finished = this.word.every(letter =>
-      this.guessedLetters.includes(letter)
+    const finished = this.word.every(
+      letter => this.guessedLetters.includes(letter) || letter === ' '
     )
 
     if (this.remainingGuesses === 0) {
