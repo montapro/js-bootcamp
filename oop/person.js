40e48705d95f1af58ff3757d3bb51422f1e68847
Markus Krause <krause.markus@icloud.com>
2019-10-15 14:26:18 +0200

    creating subclasses

--- oop/person.js
+++ oop/person.js
@@ -1,3 +1,5 @@
+// Prototypal Ineritance
+
 class Person {
   constructor(firstName, lastName, age, likes = []) {
     this.firstName = firstName
@@ -21,9 +23,34 @@ class Person {
   }
 }
 
-const me = new Person('Andrew', 'Mead', 27, ['Teaching', 'Biking'])
-me.setName('Alexis Turner')
-console.log(me.getBio())
+class Employee extends Person {
+  constructor(firstName, lastName, age, position, likes) {
+    super(firstName, lastName, age, likes)
+    this.position = position
+  }
+  getBio() {
+    return `${this.firstName} ${this.lastName} is a ${this.position}`
+  }
+  getYearsLeft() {
+    return 65 - this.age
+  }
+}
 
-const person2 = new Person('Clancey', 'Turner', 51)
-console.log(person2.getBio())
+class Student extends Person {
+  constructor(firstName, lastName, age, grade, likes) {
+    super(firstName, lastName, age, likes)
+    this.grade = grade
+  }
+  updateGrade(change) {
+    this.grade += change
+  }
+  getBio() {
+    const status = (this.grade >= 70) ? 'passing' : 'failing'
+    return `${this.firstName} is ${status} the class.`
+  }
+}
+
+me = new Student('Andrew', 'Mead', 27, 88, ['teaching', 'biking'])
+console.log(me.getBio())
+me.updateGrade(-20)
+console.log(me.getBio())
