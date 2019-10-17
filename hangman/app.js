cc1fe3c6416e3107238896c0231a9420bc5f10d1
Markus Krause <krause.markus@icloud.com>
2019-10-17 08:57:04 +0200

    http request form javascript

--- hangman/app.js
+++ hangman/app.js
@@ -1,6 +1,6 @@
 const puzzleEl = document.getElementById('puzzle')
 const guessesEl = document.getElementById('guesses')
-const game1 = new Hangman('Car parts', 2)
+const game1 = new Hangman('Nesim Alkan', 2)
 
 puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
@@ -12,3 +12,16 @@ window.addEventListener('keypress', e => {
   puzzleEl.textContent = game1.puzzle
   guessesEl.textContent = game1.statusMessage
 })
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+  if (e.target.readyState === 4) {
+    const data = JSON.parse(e.target.responseText)
+    console.log(data)
+  }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
