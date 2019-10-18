1c75f92d2d539a55af43dda49c2798020aac076d
Markus Krause <krause.markus@icloud.com>
2019-10-18 17:07:41 +0200

    callback abstraction

--- hangman/app.js
+++ hangman/app.js
@@ -7,21 +7,32 @@ guessesEl.textContent = game1.statusMessage
 
 window.addEventListener('keypress', e => {
   const guess = String.fromCharCode(e.charCode)
-  
+
   game1.makeGuess(guess)
   puzzleEl.textContent = game1.puzzle
   guessesEl.textContent = game1.statusMessage
 })
 
-// Making an HTTP request
-const request = new XMLHttpRequest()
-
-request.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4) {
-    const data = JSON.parse(e.target.responseText)
-    console.log(data)
+getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`)
+  } else {
+    console.log(puzzle)
   }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
-request.send()
+// const countryCode = 'US'
+// const countryRequest = new XMLHttpRequest()
+
+// countryRequest.addEventListener('readystatechange', e => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data2 = JSON.parse(e.target.responseText)
+//     myCountry = data2.find(country => country.alpha2Code === countryCode)
+//     console.log(myCountry.name)
+//   } else if (e.target.readyState === 4) {
+//     console.log('An error has taken place')
+//   }
+// })
+
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+// countryRequest.send()
