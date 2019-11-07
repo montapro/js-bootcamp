29b7eda4d6a278cf2b33cde85e8416de23ac13e6
Markus Krause <markus.krause@voll.digital>
2019-11-07 10:24:18 +0100

    remove protocol from urls

--- hangman/scripts/requests.js
+++ hangman/scripts/requests.js
@@ -1,5 +1,5 @@
 const getPuzzle = async wordCount => {
-  const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+  const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
 
   if (response.status === 200) {
     const data = await response.json()
@@ -15,7 +15,7 @@ const getCurrentCountry = async () => {
 }
 
 const getCountry = async countryCode => {
-  const response = await fetch('http://restcountries.eu/rest/v2/all')
+  const response = await fetch('//restcountries.eu/rest/v2/all')
 
   if (response.status === 200) {
     const data = await response.json()
@@ -26,7 +26,7 @@ const getCountry = async countryCode => {
 }
 
 const getLocation = async () => {
-  const response = await fetch('http://ipinfo.io/json?token=aac18f2c0b00c4')
+  const response = await fetch('//ipinfo.io/json?token=aac18f2c0b00c4')
 
   if (response.status === 200) {
     return response.json()
