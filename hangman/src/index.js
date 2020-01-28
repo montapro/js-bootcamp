3c4168f0bd429854bd774eb618d06a376581588a
Markus Krause <markuskrause@Markus-MacBook.fritz.box>
2020-01-28 16:15:45 +0100

    converting hangman app

---
+++ hangman/src/index.js
@@ -0,0 +1,36 @@
+import Hangman from './hangman';
+import getPuzzle from './requests';
+
+const puzzleEl = document.getElementById('puzzle');
+const guessesEl = document.getElementById('guesses');
+let game1;
+
+window.addEventListener('keypress', e => {
+  const guess = String.fromCharCode(e.charCode);
+
+  game1.makeGuess(guess);
+  render();
+});
+
+const render = () => {
+  puzzleEl.innerHTML = '';
+  guessesEl.textContent = game1.statusMessage;
+
+  game1.puzzle.split('').forEach(letter => {
+    const letterEl = document.createElement('span');
+
+    letterEl.textContent = letter;
+    puzzleEl.appendChild(letterEl);
+  });
+};
+
+const startGame = async () => {
+  const puzzle = await getPuzzle('2');
+
+  game1 = new Hangman(puzzle, 15);
+  render();
+};
+
+document.querySelector('#reset').addEventListener('click', startGame);
+
+startGame();
