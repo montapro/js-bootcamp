6393c12ebf1fde0b712784d260fd293bbbf2c8a1
Markus Krause <markuskrause@Markus-MacBook.fritz.box>
2020-01-28 15:35:16 +0100

    environments and source maps

--- boilerplate/src/utilities.js
+++ boilerplate/src/utilities.js
@@ -6,5 +6,5 @@ const add = (a, b) => a + b;
 const name = 'Markus';
 
 const square = x => x * x;
-
+console.log('from my code');
 export { add, name, square as default };
--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -17,5 +17,10 @@ module.exports = {
         }
       }
     }]
-  }
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    publicPath: '/scripts/'
+  },
+  devtool: 'source-map'
 };
