a4c0ecd406e89cc7b97d82bcafe746de2549a69b
Markus Krause <markus.krause@voll.digital>
2020-01-30 16:32:09 +0100

    converting notes app

---
+++ notes-app/src/index.js
@@ -0,0 +1,6 @@
+import { getNotes, createNote, removeNote } from './notes';
+
+// console.log(getNotes());
+// createNote();
+removeNote('13167c20-ccfc-46dc-bfce-18c7598aee6f');
+console.log(getNotes());
