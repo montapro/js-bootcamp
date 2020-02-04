28712750d13529c4630598ab03a193f1c5eabd01
Markus Krause <markus.krause@voll.digital>
2020-02-04 18:01:44 +0100

    some shit

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,8 +1,19 @@
 import { getNotes, createNote, removeNote, updateNote } from './notes';
+import { getFilters, setFilters } from './filters';
 
 // console.log(getNotes());
 // createNote();
 // removeNote('1');
 
-updateNote('')
-console.log(getNotes());
+// updateNote('78bbf571-91e7-471a-a51e-c1a46e24032c', {
+//   title: 'My note title',
+//   body: 'This is the body for my note.'
+// });
+// console.log(getNotes());
+
+console.log(getFilters());
+setFilters({
+  searchText: 'Office',
+  sortBy: 'byCreated'
+});
+console.log(getFilters());
