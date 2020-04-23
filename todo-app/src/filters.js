4c7bf1194f5fdea309adf0d02180231cd996ccb6
Markus Krause <markus.krause@voll.digital>
2020-04-23 13:42:25 +0200

    setup todo-app for node

---
+++ todo-app/src/filters.js
@@ -0,0 +1,16 @@
+const filters = {
+  searchText: '',
+  hideCompleted: false,
+};
+// Set up filters default object
+
+const getFilters = () => {};
+// getFilters
+// Arguments: none
+// Return value: filters object
+
+// setFilters
+// Arguments: updates object with optional searchText or hideCompleted
+// Return value: none
+
+// Make sure to set up the exports
